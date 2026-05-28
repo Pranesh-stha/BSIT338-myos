5bd75b1fc7dc0866a79cc5079d929d0abc8655d7
root <root@Praneshs-Lenovo-Legion-7.localdomain>
2026-05-28 01:32:44 +0000

    Restructure project: split bootloader and kernel, add FAT12 image build

--- Makefile
+++ Makefile
@@ -1,25 +1,51 @@
 ASM = nasm
-BUILD_DIR = build
+
 SRC_DIR = src
+BUILD_DIR = build
 
-.PHONY: all floppy_image kernel clean always run
+.PHONY: all floppy_image kernel bootloader clean always
 
-all: floppy_image
-
+#
+# Floppy image
+#
 floppy_image: $(BUILD_DIR)/main_floppy.img
 
-$(BUILD_DIR)/main_floppy.img: $(BUILD_DIR)/main.bin
-	cp $(BUILD_DIR)/main.bin $(BUILD_DIR)/main_floppy.img
-	truncate -s 1440k $(BUILD_DIR)/main_floppy.img
+$(BUILD_DIR)/main_floppy.img: bootloader kernel
+	dd if=/dev/zero of=$(BUILD_DIR)/main_floppy.img bs=512 count=2880
+	mkfs.fat -F 12 -n "MYOS" $(BUILD_DIR)/main_floppy.img
+	dd if=$(BUILD_DIR)/bootloader.bin of=$(BUILD_DIR)/main_floppy.img conv=notrunc
+	mcopy -i $(BUILD_DIR)/main_floppy.img $(BUILD_DIR)/kernel.bin "::kernel.bin"
 
-$(BUILD_DIR)/main.bin: always
-	$(ASM) $(SRC_DIR)/main.asm -f bin -o $(BUILD_DIR)/main.bin
+#
+# Bootloader
+#
+bootloader: $(BUILD_DIR)/bootloader.bin
 
+$(BUILD_DIR)/bootloader.bin: always
+	$(ASM) $(SRC_DIR)/bootloader/main.asm -f bin -o $(BUILD_DIR)/bootloader.bin
+
+#
+# Kernel
+#
+kernel: $(BUILD_DIR)/kernel.bin
+
+$(BUILD_DIR)/kernel.bin: always
+	$(ASM) $(SRC_DIR)/kernel/main.asm -f bin -o $(BUILD_DIR)/kernel.bin
+
+#
+# Always
+#
 always:
 	mkdir -p $(BUILD_DIR)
 
+#
+# Clean
+#
 clean:
 	rm -rf $(BUILD_DIR)/*
 
+#
+# Run
+#
 run: floppy_image
 	qemu-system-i386 -fda $(BUILD_DIR)/main_floppy.img
---
+++ src/bootloader/main.asm
@@ -0,0 +1,89 @@
+org 0x7C00
+bits 16
+
+%define ENDL 0x0D, 0x0A
+
+
+;
+; FAT12 header
+;
+jmp short start
+nop
+
+bdb_oem:                    db 'MSWIN4.1'           ; 8 bytes
+bdb_bytes_per_sector:       dw 512
+bdb_sectors_per_cluster:    db 1
+bdb_reserved_sectors:       dw 1
+bdb_fat_count:              db 2
+bdb_dir_entries_count:      dw 0E0h
+bdb_total_sectors:          dw 2880                 ; 2880 * 512 = 1.44 MB
+bdb_media_descriptor_type:  db 0F0h                 ; F0 = 3.5" floppy
+bdb_sectors_per_fat:        dw 9
+bdb_sectors_per_track:      dw 18
+bdb_heads:                  dw 2
+bdb_hidden_sectors:         dd 0
+bdb_large_sector_count:     dd 0
+
+; Extended Boot Record
+ebr_drive_number:           db 0                    ; 0x00 = floppy, 0x80 = HDD
+                            db 0                    ; reserved
+ebr_signature:              db 29h
+ebr_volume_id:              db 12h, 34h, 56h, 78h   ; serial number
+ebr_volume_label:           db 'MYOS       '        ; 11 bytes, padded with spaces
+ebr_system_id:              db 'FAT12   '           ; 8 bytes, padded with spaces
+
+
+;
+; Code
+;
+start:
+    jmp main
+
+main:
+    ; Setup data segments
+    mov ax, 0
+    mov ds, ax
+    mov es, ax
+
+    ; Setup stack
+    mov ss, ax
+    mov sp, 0x7C00
+
+    ; Print hello message
+    mov si, msg_hello
+    call puts
+
+.halt:
+    hlt
+    jmp .halt
+
+
+; puts: prints a null-terminated string
+; Input: ds:si points to the string
+puts:
+    push si
+    push ax
+    push bx
+
+.loop:
+    lodsb
+    or al, al
+    jz .done
+
+    mov ah, 0x0E
+    mov bh, 0
+    int 0x10
+
+    jmp .loop
+
+.done:
+    pop bx
+    pop ax
+    pop si
+    ret
+
+
+msg_hello: db 'Hello world from MyOS!', ENDL, 0
+
+times 510-($-$$) db 0
+dw 0AA55h
