5113de6269d76d89473ca94fc51eb1e7fd8ed203
root <root@Praneshs-Lenovo-Legion-7.localdomain>
2026-05-28 11:21:11 +0000

    Part 5: Full printf implementation with number formatting

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -6,10 +6,10 @@ void _cdecl cstart_(uint16_t bootDrive)
     puts("Hello world from C!\r\n");
 
     printf("Formatted %s: %% %c %d %i\r\n", "string", 'a', 1234, -5678);
-    printf("Hex: %x %X %p\r\n", 0xdeadbeef, 0xCAFEBABE, 0x1234);
+    printf("16-bit hex: %x %x %x\r\n", 0xbeef, 0xcafe, 0x1234);
     printf("Octal: %o\r\n", 12345);
-    printf("Long: %ld %lx\r\n", -100000000l, 0xdeadbeefl);
-    printf("Char short: %hd %hx %hhu\r\n", (short)27, (short)0x1234, (unsigned char)'A');
+    printf("32-bit long: %ld %lx\r\n", -100000000l, 0xdeadbeefl);
+    printf("Short: %hd %hx %hhu\r\n", (short)27, (short)0x1234, (unsigned char)'A');
     printf("Long long: %lld %llx\r\n", -10000000000ll, 0xdeadbeefcafebabell);
 
 end:
