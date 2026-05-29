87d84fc526cde771fe4dc847b607c7a1e3a4f5c7
root <root@Praneshs-Lenovo-Legion-7.localdomain>
2026-05-29 12:44:18 +0000

    Part 7 2/2 Phase 1: port FAT12 driver to GCC (remove far, packed structs, flat memdefs)

---
+++ src/bootloader/stage2/memdefs.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define MEMORY_FAT_ADDR   0x20000
+#define MEMORY_FAT_SIZE   0x10000
---
+++ src/bootloader/stage2/string.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "stdint.h"
+
+const char* strchr(const char* str, char chr);
+char* strcpy(char* dst, const char* src);
+unsigned strlen(const char* str);
